f187ef531cdc693b1a88f405d5b2295cd7dfa06f
Eric Vincent <eric@efvincent.com>
2019-12-16 18:43:35 -0500

    setup day 02

--- app/main.hs
+++ app/main.hs
@@ -1,7 +1,9 @@
 import Day01 (part1, part2)
+import Day02 (ans)
 
 main :: IO ()
 main = do 
     putStrLn "Hello, Advent of Code 2019"
     putStrLn $ "Day 01 part 01: " ++ show part1
     putStrLn $ "Day 01 part 02: " ++ show part2
+    putStrLn $ "Day 02        : " ++ show ans
---
+++ src/Day02.hs
@@ -0,0 +1,7 @@
+module Day02 where
+
+import AdventData (day02)
+
+mem = zip [0..] day02
+
+ans = 0
