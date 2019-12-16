01b7c6d7147fc9ce85e96048d4c0d57989d519ed
Eric Vincent <eric@efvincent.com>
2019-12-16 18:17:07 -0500

    solve day 1 parts 1 & 2

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,4 +1,18 @@
-module Day01 (rawData) where
+module Day01 where
 
-rawData :: String
-rawData = "data"
+import AdventData (day01)
+
+amounts = map (\s -> read s::Int) (lines day01)
+
+fuelReq :: Int -> Int
+fuelReq n = div n 3 - 2
+
+part1 = sum $ map fuelReq amounts
+
+deepFuelReq :: Int -> Int
+deepFuelReq n | n <= 0 = 0
+deepFuelReq n = (if req < 0 then 0 else req) + deepFuelReq req
+    where req = fuelReq n
+
+part2 = sum (map deepFuelReq amounts)
+
