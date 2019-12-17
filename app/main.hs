35ceb4e6fd9b98652798083fc5dca8eb93398bdd
Eric Vincent <eric@efvincent.com>
2019-12-17 16:35:23 -0500

    display day 2 solution in main

--- app/main.hs
+++ app/main.hs
@@ -1,9 +1,11 @@
 import Day01 (part1, part2)
-import Day02 (ans)
+import Day02 (day2part1, day2part2)
 
 main :: IO ()
 main = do 
     putStrLn "Hello, Advent of Code 2019"
     putStrLn $ "Day 01 part 01: " ++ show part1
     putStrLn $ "Day 01 part 02: " ++ show part2
-    putStrLn $ "Day 02        : " ++ show ans
+    putStrLn $ "Day 02 part 01: " ++ show day2part1
+    putStrLn $ "Day 02 part 02: " ++ show day2part2
+ 
