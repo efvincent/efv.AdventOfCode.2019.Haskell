c819cb15e3fc622b235c888b6610f9a102488586
Eric Vincent <eric@efvincent.com>
2019-12-17 16:34:54 -0500

    tidy up day 1 hiding internal apply fn

--- src/Day02.hs
+++ src/Day02.hs
@@ -9,13 +9,15 @@ replace i e xs = case splitAt i xs of
     _ -> xs
 
 -- instructions say to replace posistions 1 and 2
-memory = replace 1 12 (replace 2 2 day02)
+initMem :: Int -> Int -> [Int]
+initMem v1 v2 = replace 1 v1 (replace 2 v2 day02)
 
-apply :: (Int -> Int -> Int) -> Int -> Int -> Int -> [Int] -> [Int]
-apply f p1 p2 pAns mem = replace pAns (f (mem !! p1) (mem !! p2)) mem
+memory = initMem 12 2
 
-run :: Int -> [Int] -> [Int]
-run pos mem =
+run pos mem = 
+    let 
+        apply :: (Int -> Int -> Int) -> Int -> Int -> Int -> [Int] -> [Int]
+        apply f p1 p2 pAns mem = replace pAns (f (mem !! p1) (mem !! p2)) mem in
     if op /= 99 then run (pos + 4) (apply fn p1 p2 pAns mem) else mem
     where
         (op:p1:p2:pAns:_) = drop pos mem
