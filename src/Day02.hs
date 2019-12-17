b9c2d1d3edddacf6366659aa89f4f03d540373a5
Eric Vincent <eric@efvincent.com>
2019-12-16 19:37:25 -0500

    solve day 2 part 1

--- src/Day02.hs
+++ src/Day02.hs
@@ -9,19 +9,19 @@ replace i e xs = case splitAt i xs of
     _ -> xs
 
 -- instructions say to replace posistions 1 and 2
-memory = replace 1 12 (replace 2 2 day02)    
+memory = replace 1 12 (replace 2 2 day02)
 
 apply :: (Int -> Int -> Int) -> Int -> Int -> Int -> [Int] -> [Int]
-apply f p1 p2 pAns mem =
-    replace pAns (f v1 v2) mem
-    where
-        v1 = mem !! p1
-        v2 = mem !! p2
+apply f p1 p2 pAns mem = replace pAns (f (mem !! p1) (mem !! p2)) mem
 
 run :: Int -> [Int] -> [Int]
-run pos mem = case drop pos mem of
-    (1:p1:p2:pAns:_) -> run (pos + 4) (apply (+) p1 p2 pAns mem)
-    (2:p1:p2:pAns:_) -> run (pos + 4) (apply (*) p1 p2 pAns mem)
-    _ -> mem
+run pos mem =
+    if op /= 99 then run (pos + 4) (apply fn p1 p2 pAns mem) else mem
+    where
+        (op:p1:p2:pAns:_) = drop pos mem
+        fn = case op of
+            1 -> (+)
+            2 -> (*)
+            99 -> \ _ _ -> 0
 
-ans = 0
+ans = head $ run 0 memory
