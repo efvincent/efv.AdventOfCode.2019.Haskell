56335de6401812ac5ca9fb30d05647a935eee7b7
Eric Vincent <eric@efvincent.com>
2019-12-17 17:23:28 -0500

    refactored again

--- src/Day02.hs
+++ src/Day02.hs
@@ -15,16 +15,12 @@ initMem v1 v2 = replace 1 v1 (replace 2 v2 day02)
 memory = initMem 12 2
 
 run pos mem =
-    let
-        apply :: (Int -> Int -> Int) -> Int -> Int -> Int -> [Int] -> [Int]
-        apply f p1 p2 pAns mem = replace pAns (f (mem !! p1) (mem !! p2)) mem in
-    if op /= 99 then run (pos + 4) (apply fn p1 p2 pAns mem) else mem
+    if op /= 99 
+    then run (pos + 4) (replace pAns (fn (mem!!p1) (mem!!p2)) mem) 
+    else mem
     where
         (op:p1:p2:pAns:_) = drop pos mem
-        fn = case op of
-            1 -> (+)
-            2 -> (*)
-            99 -> \ _ _ -> 0
+        fn = case op of 1 -> (+); 2 -> (*); 99 -> \ _ _ -> 0
 
 day2part1 = head $ run 0 memory
 
