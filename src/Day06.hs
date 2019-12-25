45fe634e54dd7ff38640d5963dacb4893ab49ab2
Eric Vincent <eric@efvincent.com>
2019-12-25 02:19:53 -0500

    day 6 part 1 solution

---
+++ src/Day06.hs
@@ -0,0 +1,23 @@
+module Day06 where
+
+import           Data.List.Split (splitOn)
+import qualified Data.Map.Strict as M
+import           AdventData (day06,day06ex01)
+
+tokv :: [a] -> (a,a)      -- make key value pair out of a 2 element list, 2nd element is the key
+tokv (e1:e2:xs) = (e2,e1)
+
+d = M.fromList . map (tokv . splitOn [')']) $ lines day06
+d'= M.fromList . map (tokv . splitOn [')']) $ lines day06ex01
+
+ans  = sum . map (countChain d ) $ M.keys d
+ans' = sum . map (countChain d') $ M.keys d'
+
+countChain :: M.Map [Char] [Char] -> String -> Int
+countChain dict = loop 0
+  where
+    loop :: Int -> String -> Int
+    loop acc k =
+        case M.lookup k dict of 
+            Just next -> loop (acc + 1) next
+            Nothing -> acc
