2bccfc10931783b3eff4a5e9936f6c6ff0830bba
Eric Vincent <eric@efvincent.com>
2019-12-26 14:27:17 -0500

    day 6 part 2 solved - had one minor error

--- src/Day06b.hs
+++ src/Day06b.hs
@@ -1,11 +1,8 @@
--- Needed so that `M.Map String String` can be used instead of `M.Map [Char] [Char]`
-{-# LANGUAGE OverloadedStrings #-}
-
 module Day06b where
 
 import           Data.List.Split (splitOn)
 import           Data.List ((\\),sortOn)
-import           Control.Monad (foldM)
+import           Data.Maybe (mapMaybe)
 import qualified Data.Map.Strict as M
 import           AdventData (day06,day06ex01,day06ex02)
 
@@ -13,7 +10,7 @@ type OrbitMap = M.Map String [String]
 
 makeOrbitMap :: String -> OrbitMap
 makeOrbitMap raw =
-    fullMap
+    foldl addLink dict pairs
   where
     pairs = map (splitOn [')']) $ lines raw :: [[String]]
     dict = M.fromList . map (\(e1:e2:xs) -> (e2, [e1])) $ pairs :: OrbitMap
@@ -29,64 +26,43 @@ makeOrbitMap raw =
         case M.lookup key m of
             Just edges -> M.insert key (updateMapEntry newLink edges) m
             Nothing -> M.insert key [newLink] m
-    fullMap = foldl addLink dict pairs
+
 
 type Planet = String
-type Path = [Planet]
-type Dead = [Planet]
-data State = State { path :: Path
-                   , dead :: Dead
+data State = State { path :: [Planet]
+                   , dead :: [Planet]
                    , to   :: Planet
                    , om   :: OrbitMap
                    } deriving (Show)
 
-filterM :: [Maybe a] -> [a]
-filterM [] = []
-filterM (Nothing : xs) = filterM xs
-filterM (Just v : xs) = v : filterM xs
-
--- | [Candidates] ->
-solve1 :: [Planet] -> State -> Maybe State
-solve1 candidates state =
-    -- remove everything in the (path state) and (dead state) from candidates
+checkCandidates :: [Planet] -> State -> Maybe State
+checkCandidates candidates state =
+    -- remove everything already in path or dead from candidates
     -- if left with [] then Nothing
     -- else foldM candidates over solve2
-    ans
-  where
-    cands = candidates \\ (path state ++ dead state)
-    ans =
-        case cands of
-            [] -> Nothing
-            cands ->
-                case filterM $ map (solve2 state) cands of
-                    [] -> Nothing
-                    states -> Just $ head (sortOn path states)      -- This is no good, you need to take them all to
-                                                                    -- the solution, then see which is shortest
+    case candidates \\ (path state ++ dead state) of
+        [] -> Nothing
+        cands ->
+            case mapMaybe (solve2 state) cands of
+                [] -> Nothing
+                states -> Just $ head (sortOn path states)
 
 solve2 :: State -> Planet -> Maybe State
 solve2 state current =
     -- If the current candidate == (to state) then Just (path state)
-
     -- Append the current location to the path
     -- For the current location, get all the candidates
     -- if there are no candidates, return Nothing
-    -- otherwise, solve1 state cadidates
+    -- otherwise, checkCandidates state cadidates
+    case (current == to state, M.lookup current (om state)) of
+    (True,_) -> Just state
+    (_,Just cands') | null cands' -> Nothing
+    (_,Just cands') -> checkCandidates cands' state { path = current : path state }
+    (_,Nothing) -> Nothing
 
-    if current == to state
-    then Just state
-    else
-        newState
-      where
-        newp = current : path state
-        newState = case M.lookup current (om state) of
-            Just cands' | null cands' -> Nothing
-            Just cands' -> solve1 cands' state { path = newp }
-            Nothing -> Nothing
-
-{-
-The following finds a solution, but not the shortest solution
--}
-s = State { path = [], dead = [], to = "SAN", om = makeOrbitMap day06 }
-ja = solve1 ["YOU"] s
-(Just a) = ja
-ans = length (path a) - 1        -- -1 for "YOU" in the path
+solve :: String -> String -> String -> Int
+solve raw from to =
+    let s = State { path = [], dead = [], to = to, om = makeOrbitMap raw } in
+    let ja = checkCandidates [from] s in
+    let (Just a) = ja in
+    length (path a) - 2        -- -2 b/c we discount the actual start and destination
