0aeb4f2b79f9051ee730799f49c0d4baab9f6c64
Eric Vincent <eric@efvincent.com>
2019-12-29 17:13:56 -0500

    fix type error

--- src/Day05.hs
+++ src/Day05.hs
@@ -12,7 +12,7 @@ module Day05 where
             deriving (Show, Enum, Eq)
 
     -- | From an integer that indicates an opcode, get the opcode and the number of parameters expected
-    opOfInt :: Int -> (Op,Int)
+    opOfInt :: (Eq a, Num a, Num b, Show a) => a -> (Op, b)
     opOfInt n = case n of
          1 -> (Add,3)
          2 -> (Mult,3)
