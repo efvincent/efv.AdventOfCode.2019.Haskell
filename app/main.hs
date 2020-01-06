1108fa911a4cfdc4c88aa41eb07a3e78fb11adbf
Eric Vincent <eric@efvincent.com>
2020-01-06 01:58:26 -0500

    main -> display day13 results

--- app/main.hs
+++ app/main.hs
@@ -3,12 +3,10 @@ import Day02 (day2part1, day2part2)
 import Day08 (solve1)
 import Day09
 import Day10
+import Day13
 
 main :: IO ()
 main = do 
     putStrLn "Hello, Advent of Code 2019"
-    putStrLn $ "Day 01 part 01: " ++ show part1
-    putStrLn $ "Day 01 part 02: " ++ show part2
-    putStrLn $ "Day 02 part 01: " ++ show day2part1
-    putStrLn $ "Day 02 part 02: " ++ show day2part2
- 
+    putStrLn "Starting game run"
+    putStrLn $ "Game score is: " ++ show solve_d13_2
