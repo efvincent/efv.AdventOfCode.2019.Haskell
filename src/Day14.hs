25758fc40cad2588261a9ce56818ec2f5bc39c78
Eric Vincent <eric@efvincent.com>
2020-01-16 17:43:19 -0500

    day14 WIP

--- src/Day14.hs
+++ src/Day14.hs
@@ -4,6 +4,7 @@ import           AdventData        (day14, day14ex01, day14ex02, day14ex03,
                                     day14ex04, day14ex05)
 import           Control.Monad.Zip (mzip)
 import           Data.List.Split   (splitOn)
+import           Data.List         (find)
 import qualified Data.Map          as M
 import           Utility           (stoiM)
 
@@ -12,7 +13,6 @@ type Symbol     = String
 type Ingredient = (Quantity, Symbol)
 type Recipe     = (Ingredient, [Ingredient])
 type Cookbook   = [Recipe]
-type Cookbook1  = M.Map Symbol Recipe
 
 deser :: String -> Maybe Cookbook
 deser raw =
@@ -40,3 +40,30 @@ deserPair s =
   where
     parts = filter (/= "") $ splitOn " " s
 
+getRecipe :: Symbol -> Maybe Cookbook -> Maybe Recipe
+getRecipe sym cookbookM = 
+    do 
+        cb <- cookbookM
+        find (\((_,key),_) -> key == sym) cb
+
+batchCount :: Int -> Int -> Int
+batchCount batchSize amtNeeded =
+    let fullBatches = amtNeeded `div` batchSize in
+    if amtNeeded `mod` batchSize == 0 then fullBatches else fullBatches + 1
+
+-- this is not quite right - need to get the total amount for each ingredient, and then run it through
+-- the batches formula. IE rather than getting 4 batches of 10 of "A" because 4 different recipes need
+-- 7 "A"s, we add up all the requirements to find 28 total "A"s would be needed, and for that we can
+-- produce 3 batches and only have 2 wasted as opposed to having 40 "A" and 12 wasted.
+getOreCost :: [Ingredient] -> Maybe Cookbook -> Maybe Int
+getOreCost [(amt,"ORE")] _ = Just amt
+getOreCost ((amt,ing):ings) cb =
+    do
+        ((batchSize, curSym), curIngs) <- getRecipe ing cb
+        let batches = batchCount batchSize amt
+        curCost <- getOreCost curIngs cb
+        rest <- getOreCost ings cb
+        return $ batches * curCost + rest
+getOreCost [] _ = Just 0
+
+d = deser day14ex01
